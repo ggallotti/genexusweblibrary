8ab5617fac1df2ae303f56d3686be628cc5b7928
Gonzalo <gonzalogallotti@gmail.com>
2021-02-02 15:16:06 -0300

    Debouncing support

--- src/gx-web-extensions.js
+++ src/gx-web-extensions.js
@@ -204,6 +204,32 @@ gx.extensions.web = (function ($) {
 			}
 		},
 
+		util: {
+			debounceNotifier: function () {
+				gx.fx.obs.notify('gx.extensions.web.util.debounceevent', []);
+			},
+
+			debounce: function(wait) {
+				this.debouncerFnc = this.debouncerFnc || this.debounceImpl.apply(this, [this.debounceNotifier, wait]);
+				this.debouncerFnc();
+			},
+
+			debounceImpl: function(func, wait, immediate) {
+				var timeout;
+				return function() {
+					var context = this, args = arguments;
+					var later = function() {
+						timeout = null;
+						if (!immediate) func.apply(context, args);
+					};
+					var callNow = immediate && !timeout;
+					clearTimeout(timeout);
+					timeout = setTimeout(later, wait);
+					if (callNow) func.apply(context, args);
+				};
+			}
+		},
+
 		timer: {
 			installed: false,
 			started: false,
